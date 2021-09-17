366c08a53c9c4d768a50380e57b4e8be9328b8d8
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-09-17 13:38:08 +0900

    add seed

---
+++ hasura/migrations/default/1631853306825_insert_into_public_author/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."author"("id", "name", "updated_at", "created_at") VALUES (2, E'Jeff Beck', E'2021-09-17T04:35:06.749103+00:00', E'2021-09-17T04:35:06.749103+00:00');
---
+++ hasura/migrations/default/1631853347466_insert_into_public_books/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."books"("id", "title", "author_id", "updated_at", "created_at") VALUES (3, E'Effective TypeScript', 2, E'2021-09-17T04:35:47.461205+00:00', E'2021-09-17T04:35:47.461205+00:00');
